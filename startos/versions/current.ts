941cb50516c1a523154b267db8fe5088f8067fcd
Ali Kutlusoy <ali@kutlusoy.com>
2026-07-23 13:43:56 +0200

    Release v4.0.4

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -1,10 +1,10 @@
 import { VersionInfo } from '@start9labs/start-sdk'
 
 export const current = VersionInfo.of({
-  version: '4.0.3:0',
+  version: '4.0.4:0',
   releaseNotes: {
-    en_US: 'StartOS for Elektron Net v4.0.2:0.',
-    de_DE: 'StartOS für Elektron Net v4.0.2:0.',
+    en_US: 'StartOS for Elektron Net v4.0.4:0.',
+    de_DE: 'StartOS für Elektron Net v4.0.4:0.',
   },
   migrations: {
     up: async ({ effects }) => {},
